2a9319d7a1cff444f5b13ed3682bac602e2a7fdf
simeon1929 <simeon1929@gamil.com>
2021-10-28 21:42:25 +0530

    switch case

--- js/loop.js
+++ js/loop.js
@@ -15,3 +15,43 @@ for(let x= 0; x<10 ; x++) {
         console.log('Odd number is', x)
     }
 }
+
+
+// switch case 
+
+let days = prompt('Enter Your number ', 1)
+
+switch(days) {
+    case 1:
+        console.log('Monday Working day' );
+    break;
+
+    case 2:
+        console.log('Tuesday Working day');
+    break;
+
+    case 3:
+        console.log('Wednesday Working day');
+    break;
+
+    case 4:
+        console.log('Thursday Working day');
+    break;
+
+    case 5:
+        console.log('Friday Working day');
+    break;
+
+    case 6:
+        console.log('Saturday Working day');
+    break;
+
+    case 7:
+        console.log('Sunday Working day');
+    break;
+
+    default:
+        console.log('Sunday Working day');
+    break;
+    
+}
