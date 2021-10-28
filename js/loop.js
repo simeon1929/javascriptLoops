2c530eb58f1dab95e6b6cd3db0648a81b8a5bc9a
simeon1929 <simeon1929@gamil.com>
2021-10-28 21:00:57 +0530

    for loop

--- js/loop.js
+++ js/loop.js
@@ -1,3 +1,17 @@
+//
+// for loops
+
+//print 10 Numbers
 
 
-alert('javascript')
+for(let x= 0; x<10 ; x++) {
+    //console.log('number is ', x)
+
+    // find odd numer 
+
+    if (x%2==0) {
+        console.log('Even number is', x)
+    } else if(x%2 ==1) {
+        console.log('Odd number is', x)
+    }
+}
