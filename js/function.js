b51bdfbe282fdf3ba024bc584146f3b5a53f9a2b
simeon1929 <simeon1929@gamil.com>
2021-10-29 16:44:34 +0530

    function Started

--- js/function.js
+++ js/function.js
@@ -2,11 +2,19 @@ console.log('Function started');
 
 
 // Basic function
-function myStart(x) { 
-    //console.log('basic function');
-    sum = x+x
-    return false
+function myStart() { 
+    console.log('basic function');
 }
 
-myStart(10)
+myStart()
 
+
+//function statrted
+
+function myFun(FirstName, LastName, age){
+    return 'My First Name is ' + FirstName + 'and Last Name is '+ LastName +  age
+}
+
+const funCall = myFun('Beulah', 'simeon', 25 )
+
+console.log(funCall);
