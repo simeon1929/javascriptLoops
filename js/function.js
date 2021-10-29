6ade3bf08000a7759cc34959ec42260861d697bc
simeon1929 <simeon1929@gamil.com>
2021-10-29 17:13:14 +0530

    functions cleard

--- js/function.js
+++ js/function.js
@@ -18,3 +18,41 @@ function myFun(FirstName, LastName, age){
 const funCall = myFun('Beulah', 'simeon', 25 )
 
 console.log(funCall);
+
+
+// add two function
+
+function add(x, y){
+    return sum = x + y  
+}
+
+ add(10,30)
+
+console.log( 'Result is ',sum);
+
+
+// Another type
+function minusFun(a , b) {
+    return minus = a-b
+}
+
+let reuslt = minusFun(10, 5);
+
+console.log('Your second result', reuslt);
+
+
+// Function conditions
+
+function condi(name, age){
+
+    if(name, age){
+        return 'your name is ' + name + 'and your age is ' + age
+    } else {
+        return 'Enter your correct name'
+    }
+    console.log('Name and age is perfect');
+}
+
+let resultCod = condi('simeon', 26)
+
+console.log(resultCod);
