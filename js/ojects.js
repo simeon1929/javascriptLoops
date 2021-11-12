17b593ff2afed0b5fe0ae9bcda797a7f9642dc83
simeon1929 <simeon1929@gamil.com>
2021-11-12 22:46:57 +0530

    define property

--- js/ojects.js
+++ js/ojects.js
@@ -29,15 +29,75 @@ let myobj = {
     },
     addFun : ()=>{
         console.log('My age is '+ myobj.age)
-    }
-
-    
+    }  
 }
 
+let fun1 = ()=>{
+    console.log('helllo');
+}
 
+fun1()
 myobj.haifyun()
 myobj.addFun()
 
 
 // console.log(obj.sayHi());
+
+//Square broket symbol 
+
+// function example (){
+//     console.log('example function');
+// }
+
+// example()
+
+// Fname['first name'] = 'simeon Haries'
+
+// console.log(Fname);
+
+// salary 
+
+var employee1 = {
+    name : 'simeon',
+    desigination : 'Front end developer ',
+    salary : 25000
+}
+
+console.log(employee1.salary);
+
+employee1.salary = 35000
+
+console.log(employee1.salary);
+
+var employe2 = {
+    name : 'Simeon Haries',
+    salary :20000
+
+}
+
+console.log(employe2.name);
+console.log(employe2.salary);
+
+Object.defineProperty(employe2, 'name', {
+    configurable: true,
+    writable: false,
+    enumerable: true,
+    value : 'Simeon'
+})
+
+
+Object.defineProperty(employe2, 'salary', {
+    configurable :true,
+    writable : false,
+    enumerable : true,
+    value : 50
+})
+employe2.name ="Beulah",
+
+employe2.salary = 30000
+
+console.log(employe2.name);
+
+console.log(employe2.salary);
+
 
