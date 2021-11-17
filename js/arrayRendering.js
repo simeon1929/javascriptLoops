2d0e0a38e5528b473d573d313ffa772a72ddcea9
Simeon Haries <simeon@voxforem.com>
2021-11-17 23:58:37 +0530

    splice method

--- js/arrayRendering.js
+++ js/arrayRendering.js
@@ -77,4 +77,27 @@ console.log('marrage family member', addFamilymember);
 var finalFamilymember = addFamilymember.concat('Evaniya', ['boy baby', 'boy baby'])
 console.log('final family member', finalFamilymember);
 
+// slice method 
+console.log('slice method is started');
+var names = ['aa', 'bb','cc', 'dd','ee', 'ff']
+console.log('Names ', names);
+var slicemethod1 = names.slice(0,3)
+console.log('slice method betwen', slicemethod1);
+var names2 = ['aa', 'bb','cc', 'dd','ee', 'ff']
+var sliceMethod2 = names2.slice(3)
+console.log('slice method two 3 after', sliceMethod2);
+
+
+// splice del method
+var newNum = [1,2,3,4,5,6,7]
+console.log('your number is ', newNum);
+var spiceMeth1 = newNum.splice(2,3)
+console.log('after spice method ', spiceMeth1);
+var newNum1 = [1,2,3,4,5,6,7]
+var spliceMethod2 = newNum1.splice(3, newNum1.length-3)
+console.log('Your splice method is ', spliceMethod2);
+// splice del and add method
+var newNum4 = [1,2,3,4,5,6,7]
+var spiceMethod3 = newNum4.splice( 0, 0, 'simeon')
+console.log('Your splice method3 is ' , spiceMethod3);
 
