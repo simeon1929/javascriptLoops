259d3b142fe70268b881d1d7a7bb7d6b2b410c50
Simeon Haries <simeon@voxforem.com>
2021-11-26 22:50:26 +0530

    for eaach method

---
+++ js/arrayItrationMethod.js
@@ -0,0 +1,24 @@
+var fruites = ['apple', 'mango', 'papaya']
+var fruites2 = ['apple', 'mango', 'papaya']
+console.log('your array is ', fruites);
+
+// for(let i=0; i<fruites.length; i++) {
+//     console.log(i);
+// }
+
+// for each method 
+
+// fruites.forEach((item , index, array)=>{
+//     console.log('array friutes is ', array);
+//     console.log('Index friutes is ', index);
+//     console.log('item friutes is ', item);
+// })
+
+
+
+console.log('your array is ', fruites);
+fruites2.forEach((item, index, array )=>{
+     console.log('Array is ',array );
+    console.log('Index is ', index);
+    console.log('Item is ', item);
+})
